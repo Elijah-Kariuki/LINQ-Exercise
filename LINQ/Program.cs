b6a481d96fcc93d5d134a38f5ab2cb5790e9e294
Elijah Kariuki <ElijahKariuki1981@outlook.com>
2022-10-26 09:06:43 -0400

    Initial Commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace LINQ
+﻿using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+namespace LINQ
 {
     public class Program
     {
+       
+
+        
+        
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> games = new List<string>()
+        {
+            "Resident Evil Village: Shadow of Rose", "NFL John Madden Football 2023",
+            "The Legend of Zelda: Breath of the Wild","Red Dead Redemption 2"
+        };
+            IEnumerable<string> byLength = games.OrderByDescending(title => title.Length);
+            foreach (string title in byLength)
+            { Console.WriteLine(title); }
+
+            
         }
     }
 }
